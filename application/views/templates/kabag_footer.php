745a849fa4604c5e0e40625067c9525fc3a91132
Raka Sakti <begawanraka97@gmail.com>
2021-07-29 19:48:47 +0700

    baru

--- application/views/templates/kabag_footer.php
+++ application/views/templates/kabag_footer.php
@@ -38,7 +38,7 @@
       </div>
   </div>
 
-  <!-- Delete Modal-->
+  <!-- Delete Modal -->
   <div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
       <div class="modal-dialog" role="document">
           <div class="modal-content">
@@ -51,7 +51,7 @@
               <div class="modal-body">Klik tombol "Delete" jika kamu yakin untuk menghapus penilaian karyawan ini.</div>
               <div class="modal-footer">
                   <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                  <a class="btn btn-danger" href="">Delete</a>
+                  <a class="btn btn-danger" href="<?= base_url('kepala_bagian/delete_penilaian'); ?>">Delete</a>
 
               </div>
           </div>
