82fb0fd9df02ced5f4533f43f86ab1fb1ecb783a
Raka Sakti <begawanraka97@gmail.com>
2021-08-29 20:42:03 +0700

    update terkini

---
+++ application/views/hrd/kelola_penilaian_karyawan.php
@@ -0,0 +1,55 @@
+<!-- Begin Page Content -->
+<div class="container-fluid">
+    <!-- Page Heading -->
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item active" aria-current="page">Kelola Penilaian Karyawan</li>
+        </ol>
+    </nav>
+    <h1 class="h3 mb-4 text-gray-800"></h1>
+
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-info">Kriteria Karyawan</h6>
+        </div>
+        <div class="card-body">
+            <a class="btn btn-success btn-sm rounded-0 mb-3" type="a" data-toggle="tooltip" data-placement="top" title="Hitung Penilaian" href="<?= base_url('hrd/hitung_penilaian'); ?>"><i class="fas fa-tools"></i></a>
+            <table id="example" class="table table-striped table-bordered" style="width:100%">
+                <thead class="thead-dark">
+                    <tr>
+                        <th scope="col">No</th>
+                        <th scope="col">Nama Karyawan</th>
+                        <th scope="col">Tanggung Jawab</th>
+                        <th scope="col">Kepribadian</th>
+                        <th scope="col">Absensi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row"></th>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+</div>
+<!-- /.container-fluid -->
+
+</div>
+<!-- End of Main Content -->
--- application/views/templates/hrd_sidebar.php
+++ application/views/templates/hrd_sidebar.php
@@ -28,6 +28,9 @@
               <a class="nav-link" href="<?= base_url('hrd/kelola_sub_kriteria'); ?>">
                   <i class="fas fa-passport"></i>
                   <span>Kelola Sub Kriteria</span></a>
+              <a class="nav-link" href="<?= base_url('hrd/kelola_penilaian_karyawan'); ?>">
+                  <i class="fas fa-passport"></i>
+                  <span>Kelola Penilaian Karyawan</span></a>
               <a class="nav-link" href="<?= base_url('hrd/kelola_karyawan'); ?>">
                   <i class="fas fa-users-cog"></i>
                   <span>Kelola Karyawan</span></a>
